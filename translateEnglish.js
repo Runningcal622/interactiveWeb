9cc8aeb44bc347ecd798a44843186c349b55a7aa
Caleb T. Vanorio <caleb.vanorio@centre.edu>
2019-02-07 15:38:58 -0500

    update

--- translateEnglish.js
+++ translateEnglish.js
@@ -20,3 +20,24 @@ var toRussian = function()
   var eng = document.getElementById("greeting");
   eng.innerText = "Привет";
 }
+var toEnglish2 = function()
+{
+  var eng = document.getElementById("goodbye");
+  eng.innerText = "Goodbye";
+}
+
+var toFrench2 = function()
+{
+  var eng = document.getElementById("goodbye");
+  eng.innerText = "Au revoir";
+}
+var toSpanish2 = function()
+{
+  var eng = document.getElementById("goodbye");
+  eng.innerText = "Adiós";
+}
+var toRussian2 = function()
+{
+  var eng = document.getElementById("goodbye");
+  eng.innerText = "До свидания";
+}
