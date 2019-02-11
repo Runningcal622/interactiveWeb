5495e6df714d769abc3551bcbd72a7b684571045
Caleb T. Vanorio <caleb.vanorio@centre.edu>
2019-02-10 20:45:22 -0500

    made into 2 functions

--- translateEnglish.js
+++ translateEnglish.js
@@ -1,43 +1,12 @@
 
-var toEnglish = function()
+function toEnglish(str)
 {
   var eng = document.getElementById("greeting");
-  eng.innerText = "Hello";
+  eng.innerText = str;
 }
 
-var toFrench = function()
-{
-  var eng = document.getElementById("greeting");
-  eng.innerText = "Bonjour";
-}
-var toSpanish = function()
-{
-  var eng = document.getElementById("greeting");
-  eng.innerText = "Hola";
-}
-var toRussian = function()
-{
-  var eng = document.getElementById("greeting");
-  eng.innerText = "Привет";
-}
-var toEnglish2 = function()
+function toEnglish2(str)
 {
   var eng = document.getElementById("goodbye");
-  eng.innerText = "Goodbye";
-}
-
-var toFrench2 = function()
-{
-  var eng = document.getElementById("goodbye");
-  eng.innerText = "Au revoir";
-}
-var toSpanish2 = function()
-{
-  var eng = document.getElementById("goodbye");
-  eng.innerText = "Adiós";
-}
-var toRussian2 = function()
-{
-  var eng = document.getElementById("goodbye");
-  eng.innerText = "До свидания";
+  eng.innerText = str;
 }
